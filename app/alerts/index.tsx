95becd8ba5eacd967b22686fee63560f0ea367e9
Frank Gutierrez <fgutiefe@emeal.nttdata.com>
2026-05-25 22:43:52 -0500

    feat: Implement alerts

--- app/alerts/index.tsx
+++ app/alerts/index.tsx
@@ -1,10 +1,51 @@
-import { View, Text } from 'react-native';
+import ThemedButton from '@/presentation/shared/ThemedButton';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { Alert } from 'react-native';
 
 const AlertsScreen = () => {
+  const createTwoButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {
+        text: 'OK', 
+        onPress: () => console.log('OK Pressed')
+      },
+    ]);
+
+  const createThreeButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Ask me later',
+        onPress: () => console.log('Ask me later pressed'),
+      },
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {
+        text: 'OK', 
+        onPress: () => console.log('OK Pressed')
+      },
+    ]);
+    
   return (
-    <View>
-      <Text>AlertsScreen</Text>
-    </View>
+    <ThemedView margin className='justify-center'>
+      <ThemedButton 
+        children={'2-Button Alert'} 
+        onPress={createTwoButtonAlert}
+        className='mb-4' 
+      />
+      <ThemedButton 
+        children={'3-Button Alert'} 
+        onPress={createThreeButtonAlert} 
+      />
+    </ThemedView>
   );
 };
+
 export default AlertsScreen;
